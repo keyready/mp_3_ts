62d7e9aab5c25c766b537222ce7a4cde4a83c2de
keyready <vip.korchak@list.ru>
2023-01-26 18:48:41 +0300

    IM-003-12-2: тест деплой

--- client/webpack.config.ts
+++ client/webpack.config.ts
@@ -6,13 +6,13 @@ export default (env: BuildEnv) => {
     const mode = env.mode || 'development';
     const isDev = mode === 'development';
     const port = env.port || 3000;
-    const apiUrl = env.apiUrl || 'http://localhost:9999';
+    const apiUrl = env.apiUrl || 'http://valiko666.site';
 
     return buildWebpackConfig({
         mode,
         paths: {
             entry: path.resolve(__dirname, 'src'),
-            dist: path.resolve(__dirname, 'dist'),
+            dist: path.resolve(__dirname, '../dist'),
             html: path.resolve(__dirname, 'public/index.html'),
             src: path.resolve(__dirname, 'src'),
         },
