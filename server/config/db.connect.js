378970f4d29a5474dbd93a32133d7cc2b404e49d
VALIKO666 <k0fanov36@yandex.ru>
2023-01-27 13:48:54 +0300

    еще одна попытка деплоя

--- server/config/db.connect.js
+++ server/config/db.connect.js
@@ -1,6 +1,6 @@
 const {Sequelize} = require('sequelize');
 module.exports = new Sequelize('bp', 'k0fanov36', 'k0fanov36', {
     dialect: 'postgres',
-    host: 'localhost',
+    host: 'db',
     port: 5432
 });
