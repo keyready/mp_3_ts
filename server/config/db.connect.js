d5bc84d4ecba0d534f308583901a4e40d8b789fa
VALIKO666 <k0fanov36@yandex.ru>
2023-01-27 13:56:00 +0300

    dist!!!

--- server/config/db.connect.js
+++ server/config/db.connect.js
@@ -1,6 +1,7 @@
 const {Sequelize} = require('sequelize');
 module.exports = new Sequelize('bp', 'k0fanov36', 'k0fanov36', {
     dialect: 'postgres',
-    host: 'db',
+    //host: 'db',
+    host:'localhost',
     port: 5432
 });
